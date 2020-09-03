9c017a4cb11391be6bfe4fc5664ae75f1ec2bbdc
Novica Parausic <novica.parausic@hotmail.com>
2020-09-03 14:18:54 +0200

    initial commit

---
+++ src/main/java/com/micro/librarymicroservices/LibraryMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.micro.librarymicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micro/librarymicroservices/LibraryMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.librarymicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
