b8b1311bbfe3c43f9e6a8119effb7b6b735f5c1c
Novica Parausic <novica.parausic@hotmail.com>
2020-09-03 15:47:03 +0200

    commit message 346

---
+++ library-discovery/src/main/java/com/discovery/DicoveryApplication.java
@@ -0,0 +1,12 @@
+package com.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicoveryApplication.class, args);
+	}
+}
